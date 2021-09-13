0891b203c6614e8adc0cc3e410ff9da359f548ce
Mujib <mujib517@gmail.com>
2021-09-13 12:10:12 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// variables: memory(RAM) location
+// temporary 
+// primary, secondary
+// concepts(js,java,c#), syntax
+// data types
+// lang agnostic
+// var age = 10; // number
+// var name = 'abc'; // string
+// var addr = ''; // string 
+// var rollNo = 123; // number
+// var isPassed = true; // true, false
+// var balance = 1000.50; // number
+// // object, NaN, null, undefined
+// // typeof 
+// var y = null;
+// toggle comment: Ctrl + /
+// var x = 10; 
+// console.log(typeof x );
+// x = 'abc';
+// x = true;
+// declaration
+// init
+
+// int x = 10.20;
+// LHS = RHS
+// x = "abc"; // compilation error
+// operators : Arithmatic operator (+,-,*,/,%, ++, --)
+// age = age + 1; // ++age; age++;
+// x+2y = 1
+var x = 10;
+var y = 2;
+var res = x%2;
+
+
+console.log(res);
+
+
+
+
+
+
+
